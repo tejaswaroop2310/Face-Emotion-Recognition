e345803e606ca3f3d15718a9caa0fa6dde52e0c7
Swaroop Piduguralla <77975029+tejaswaroop2310@users.noreply.github.com>
2022-01-22 17:26:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfigura
 # load model
 emotion_dict = {0:'angry', 1 :'happy', 2: 'neutral', 3:'sad', 4: 'surprise'}
 # load json and create model
-json_file = open("C:/Users/AC/PycharmProjects/FaceEmotionRecognition/emotion_model1.json", 'r')
+json_file = open("emotion_model1.json", 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 classifier = model_from_json(loaded_model_json)
 
 # load weights into new model
-classifier.load_weights("C:/Users/AC/PycharmProjects/FaceEmotionRecognition/emotion_model1.h5")
+classifier.load_weights('emotion_model1.h5')
 
 #load face
 try:
-    face_cascade = cv2.CascadeClassifier('C:/Users/AC/PycharmProjects/FaceEmotionRecognition/haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 except Exception:
     st.write("Error loading cascade classifiers")
 
